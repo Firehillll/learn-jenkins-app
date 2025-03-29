05eb2ada97e72d80e97a636a871e3ed3147b8263
Firehillll <firehillll7@gmail.com>
2025-03-29 13:49:38 +0100

    Bug fix

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn Jenkins link', () => {
+test('Hilario Jenkins Site', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn Jenkins/i);
   expect(linkElement).toBeInTheDocument();
